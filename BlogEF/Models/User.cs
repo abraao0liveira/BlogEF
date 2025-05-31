12f1b6d1bf2d0ac65fe4e51b36638f9f4fd61f92
Abraão Oliveira <113057132+abraao0liveira@users.noreply.github.com>
2025-05-31 02:44:06 -0300

    add User Model

---
+++ BlogEF/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace BlogEF.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public required string PasswordHash { get; set; }
+    public required string Image { get; set; }
+    public required string Slug { get; set; }
+}
