58ebffa96294af3b8200d68bfd63582e8e60e62f
Abraão Oliveira <113057132+abraao0liveira@users.noreply.github.com>
2025-05-31 03:50:27 -0300

    create CRUD in Tag

--- BlogEF/Models/Tag.cs
+++ BlogEF/Models/Tag.cs
@@ -1,8 +1,13 @@
-﻿namespace BlogEF.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace BlogEF.Models;
+
+[Table("tag")]
 public class Tag
 {
+    [Key]
     public int Id { get; set; }
-    public required string Name { get; set; }
-    public required string Slug { get; set; }
+    [Column("name")][MaxLength(80)] public required string Name { get; set; }
+    [Column("slug")][MaxLength(80)] public required string Slug { get; set; }
 }  
