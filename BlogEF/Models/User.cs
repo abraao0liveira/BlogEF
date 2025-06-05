e5667dbc27423f447d4f91c958c385078537c300
Abraão Oliveira <113057132+abraao0liveira@users.noreply.github.com>
2025-06-05 19:50:57 -0300

    update models and Program

--- BlogEF/Models/User.cs
+++ BlogEF/Models/User.cs
@@ -1,7 +1,11 @@
-﻿namespace BlogEF.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogEF.Models;
 
 public class User
 {
+    [Key][DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     public required string Name { get; set; }
     public required string Email { get; set; }
--- BlogEF/Program.cs
+++ BlogEF/Program.cs
@@ -8,64 +8,105 @@ class Program
 {
     static void Main(string[] args)
     {
-        using (var context = new BlogDataContext())
+        using var context = new BlogDataContext();
+
+        // var user = new User
+        // {
+        //     Name = "",
+        //     Email = "",
+        //     PasswordHash = "",
+        //     Image = "",
+        //     Slug = ""
+        // };
+        //
+        // var category = new Category
+        // {
+        //     Name = "",
+        //     Slug = ""
+        // };
+        //
+        // var post = new Post
+        // {
+        //     Author = user,
+        //     Category = category,
+        //     Title = "",
+        //     Summary = "",
+        //     Body = "",
+        //     Slug = "",
+        //     CreateDate = DateTime.Now,
+        //     LastUpdateDate = DateTime.Now
+        // };
+        //
+        // context.Posts.Add(post);
+        // context.SaveChanges();
+        
+        var posts = context
+            .Posts
+            .AsNoTracking()
+            .Include(p => p.Author)
+            .OrderByDescending(p => p.LastUpdateDate)
+            .ToList();
+
+        foreach (var post in posts)
         {
-            //create
-            var tagInsert = new Tag
-            {
-                Name = "Dapper",
-                Slug = "dapper"
-            };
-            context.Tags.Add(tagInsert);
-            context.SaveChanges(); //so salva no banco com o SaveChanges()
-            
-            //update
-            var tagUpdate = context.Tags.FirstOrDefault(t => t.Id == 2); //pega a versao da tag mais atual por id trakeada(com metadados que ajudam no update ou delete)
-            if (tagUpdate == null)
-                return;
-            tagUpdate.Name = ".NET";
-            tagUpdate.Slug = "dotnet";
-            
-            context.Update(tagUpdate);
-            context.SaveChanges();
-            
-            // //delete
-            var tagDelete = context.Tags.FirstOrDefault(t => t.Id == 2);
-            if (tagDelete == null)
-                return;
-            
-            context.Remove(tagDelete);
-            context.SaveChanges();
-            
-            //list all
-            var tags = context
-                .Tags
-                .AsNoTracking() //desabilita o trakeamento, tornando mais perfomatico(ideal para listagens)
-                .ToList(); //executa a query
-            foreach (var tag in tags)
-            {
-                Console.WriteLine(tag.Name);
-            }
-            
-            //query com where
-            var tagsByName = context
-                .Tags
-                .AsNoTracking()
-                .Where(t => t.Name.Contains(".NET"))
-                .ToList(); //listar sempre ao final, performatico, pois executa a query apos usar o where, nao passando por todos os dados do banco
-            foreach (var tag in tagsByName)
-            {
-                Console.WriteLine(tag.Name);
-            }
-            
-            //read by id
-            var tagById = context
-                .Tags
-                .AsNoTracking()
-                .FirstOrDefault(t => t.Id == 2); //pega o primeiro ou null
-                //.SingleOrDefault() pega o item especifico, se tiver mais de um da erro
-            
-            Console.WriteLine(tagById?.Name);
+            Console.WriteLine(post.Author);
         }
+
+        // //create
+        // var tagInsert = new Tag
+        // {
+        //     Name = "Dapper",
+        //     Slug = "dapper"
+        // };
+        // context.Tags.Add(tagInsert);
+        // context.SaveChanges(); //so salva no banco com o SaveChanges()
+        //
+        // //update
+        // var tagUpdate = context.Tags.FirstOrDefault(t => t.Id == 2); //pega a versao da tag mais atual por id trakeada(com metadados que ajudam no update ou delete)
+        // if (tagUpdate == null)
+        //     return;
+        // tagUpdate.Name = ".NET";
+        // tagUpdate.Slug = "dotnet";
+        //
+        // context.Update(tagUpdate);
+        // context.SaveChanges();
+        //
+        // // //delete
+        // var tagDelete = context.Tags.FirstOrDefault(t => t.Id == 2);
+        // if (tagDelete == null)
+        //     return;
+        //
+        // context.Remove(tagDelete);
+        // context.SaveChanges();
+        //
+        // //list all
+        // var tags = context
+        //     .Tags
+        //     .AsNoTracking() //desabilita o trakeamento, tornando mais perfomatico(ideal para listagens)
+        //     .ToList(); //executa a query
+        // foreach (var tag in tags)
+        // {
+        //     Console.WriteLine(tag.Name);
+        // }
+        //
+        // //query com where
+        // var tagsByName = context
+        //     .Tags
+        //     .AsNoTracking()
+        //     .Where(t => t.Name.Contains(".NET"))
+        //     .ToList(); //listar sempre ao final, performatico, pois executa a query apos usar o where, nao passando por todos os dados do banco
+        // foreach (var tag in tagsByName)
+        // {
+        //     Console.WriteLine(tag.Name);
+        // }
+        //
+        // //read by id
+        // var tagById = context
+        //     .Tags
+        //     .AsNoTracking()
+        //     .FirstOrDefault(t => t.Id == 2); //pega o primeiro ou null
+        //     //.SingleOrDefault() pega o item especifico, se tiver mais de um da erro
+        //
+        // Console.WriteLine(tagById?.Name);
     }
 }
