0e0ebe3e24b14b58fa293c9e392fa1350c2d1222
Abraão Oliveira <113057132+abraao0liveira@users.noreply.github.com>
2025-08-12 23:20:50 -0300

    update

---
+++ BlogEF/Data/Mappings/CategoryMap.cs
@@ -0,0 +1,39 @@
+﻿using BlogEF.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BlogEF.Data.Mappings
+{
+    public class CategoryMap : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            //Tabela
+            builder.ToTable("category");
+
+            //Chave primaria
+            builder.HasKey(c => c.Id);
+            //Identity
+            builder.Property(c => c.Id) //indica as propriedades 
+                .ValueGeneratedOnAdd() //indica que o valor é gerado automaticamente
+                .UseIdentityColumn(); //identity(1,1)
+
+            //Propriedades
+            builder.Property(c => c.Name)
+                .IsRequired() //indica que é obrigatorio
+                .HasColumnName("name") //indica o nome da coluna
+                .HasColumnType("nvarchar") //indica o tipo da coluna
+                .HasMaxLength(80); //indica o tamanho maximo
+            builder.Property(c => c.Slug)
+                .IsRequired() //indica que é obrigatorio
+                .HasColumnName("slug") //indica o nome da coluna
+                .HasColumnType("nvarchar") //indica o tipo da coluna
+                .HasMaxLength(80); //indica o tamanho maximo
+
+            //Indices
+            builder.HasIndex(s => s.Slug, "ix_category_slug") //cria um indice para a coluna slug
+                .IsUnique(); //indica que o indice é unico
+
+        }
+    }
+}
--- BlogEF/Models/Category.cs
+++ BlogEF/Models/Category.cs
@@ -1,15 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿namespace BlogEF.Models;
 
-namespace BlogEF.Models;
-
-[Table("category")]
 public class Category
 {
-    [Key][DatabaseGenerated(DatabaseGeneratedOption.Identity)] //informa que a chave é gerada no banco 
     public int Id { get; set; }
-    [Required][MaxLength(80)][Column("name", TypeName = "nvarchar")]
     public required string Name { get; set; }
-    [Required][MaxLength(80)][Column("name", TypeName = "varchar")]
     public required string Slug { get; set; }
+    public required IList<Post> Posts { get; set; } 
 }
